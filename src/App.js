75992aa437f4995f3d571aa07462b61366e42aa1
laura <laura_kenny85@hotmail.com>
2018-12-07 13:50:26 +0000

    sounds now play when matching key is pressed

--- src/App.js
+++ src/App.js
@@ -3,9 +3,21 @@ import './app.css';
 import drumData from './drumData.json';
 
 class App extends Component {
+
+  componentDidMount() {
+    document.addEventListener("keydown", this.handleKeyPress.bind(this));
+  }
+
   playSound(id) {
     document.getElementById(id).play();
   }
+  
+  handleKeyPress(event) {
+    if(document.getElementById(event.key.toUpperCase()) != null) {
+      document.getElementById(event.key.toUpperCase()).play();
+    }
+  }
+
   render() {
     return (
       <div>
@@ -15,7 +27,7 @@ class App extends Component {
               <div className="row">
                 {drumData.map((drumPad) => {
                   return (
-                    <div className="col-md-4 drum-pad" key={drumPad.id}>
+                    <div className="col-md-4 drum-pad" key={drumPad.id} onKeyPress={this.handleKeyPress.bind(this)}>
                       <button type="button" className="btn btn-outline-secondary btn-block" onClick={this.playSound.bind(this, drumPad.id)}>{drumPad.id}
                         <audio id={drumPad.id} src={drumPad.src}></audio>
                       </button>
